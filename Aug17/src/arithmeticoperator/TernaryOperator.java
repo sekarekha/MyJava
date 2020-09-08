d95080b13c9ef402c8c6d6656060788c45de17a4
sekarekha <sekarekha@gmail.com>
2020-09-07 23:24:03 -0400

    program

---
+++ Aug17/src/arithmeticoperator/TernaryOperator.java
@@ -0,0 +1,10 @@
+package arithmeticoperator;
+
+public class TernaryOperator {
+    public static void main(String args[]) {
+        double number = -5.5;
+        String result;
+        result = (number > 0.0) ? "Positive" : "Negative";
+        System.out.println(number + " is " + result);
+    }
+}
