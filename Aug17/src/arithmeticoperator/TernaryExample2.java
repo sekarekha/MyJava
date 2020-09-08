7b2dd4fb43c639170b4b32bf384f9f2fc1bb9f74
sekarekha <sekarekha@gmail.com>
2020-09-07 23:38:22 -0400

    program

---
+++ Aug17/src/arithmeticoperator/TernaryExample2.java
@@ -0,0 +1,15 @@
+package arithmeticoperator;
+
+public class TernaryExample2 {
+    public static void main(String args[]) {
+        int i, k;
+        i = 10;
+        k = i < 0 ? -i : i;
+        System.out.print("Absolute value of ");
+        System.out.println(i + " is " + k);
+        i = -10;
+        k = i < 0 ? -i : i;
+        System.out.print("Absolute value of ");
+        System.out.println(i + " is " + k);
+    }
+}
