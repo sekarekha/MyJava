0fabaf4b659a9a5ad557f99c4e396c7c135748f5
sekarekha <sekarekha@gmail.com>
2020-10-01 20:50:29 -0400

    program

---
+++ Aug17/src/october/Break.java
@@ -0,0 +1,12 @@
+package october;
+
+public class Break {
+    public static void main(String args[]) {
+        for (int i = 1; i <= 10; i++) {
+            if (i == 6) {
+                break;
+            }
+            System.out.println(i);
+        }
+    }
+}
