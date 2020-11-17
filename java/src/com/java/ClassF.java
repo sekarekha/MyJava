43f81e316a5b65590875eccddb414d1328c68720
sekarekha <sekarekha@gmail.com>
2020-11-16 19:03:55 -0500

    new program

---
+++ java/src/com/java/ClassF.java
@@ -0,0 +1,15 @@
+package com.java;
+
+public class ClassF {
+	int a=10;
+	String val="java";
+	void display()
+	{
+		System.out.println("Hello Java");
+	}
+	public static void main(String[] args) {
+		ClassF aobj=new ClassF();
+		aobj.display();
+	}
+
+}
